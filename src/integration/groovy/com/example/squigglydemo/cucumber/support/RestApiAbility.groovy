fe8f6b6b9a5bb93a970188ada9f4ddb8a13b2bba
tinylinden <lukasz.lipka@gmail.com>
2021-11-07 17:10:20 +0100

    API versioning with vendor media type

--- src/integration/groovy/com/example/squigglydemo/cucumber/support/RestApiAbility.groovy
+++ src/integration/groovy/com/example/squigglydemo/cucumber/support/RestApiAbility.groovy
@@ -6,10 +6,14 @@ trait RestApiAbility {
 
     private final static RESTClient HTTP = new RESTClient("http://localhost:8080")
 
-    String executeGet(String path, Map<String, String> query) {
+    String executeGet(
+            String path,
+            Map<String, String> query,
+            String accept = "application/vnd.example.public.v1+json") {
         HTTP.get(
-            path: path,
-            query: query
+                path: path,
+                query: query,
+                accept: accept,
         ).getContentAsString()
     }
 }
